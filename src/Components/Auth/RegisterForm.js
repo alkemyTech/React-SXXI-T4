7f7f037d62a0e6a81ba06743d79a5f712186c730
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2022-10-29 17:45:09 -0300

    formulario de registro
    se realizaron las validaciones correspondientes mediante yup y regex,

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,49 +1,103 @@
-import React, { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import "../FormStyles.css";
 
 const RegisterForm = () => {
-	const [initialValues, setInitialValues] = useState({
-		name: "",
-		lastName: "",
+	// const [userLogin, setUserLogin] = useState({});
+
+	const navigate = useNavigate();
+
+	const validationSchema = yup.object().shape({
+		email: yup.string().required("Este campo de obligatorio"),
+		password: yup
+			.string()
+			.matches(
+				/^.*(?=.{6,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+				"La contraseña debe tener al menos una letra, un numero y simbolo, mas 6 caracteres"
+			),
+		confirmPassword: yup
+			.string()
+			.required("Please confirm your password")
+			.oneOf([yup.ref("password"), null], "Las contraseñas deben coincidir"),
 	});
 
-	const handleChange = e => {
-		if (e.target.name === "name") {
-			setInitialValues({ ...initialValues, name: e.target.value });
-		}
-		if (e.target.name === "lastName") {
-			setInitialValues({ ...initialValues, lastName: e.target.value });
-		}
-	};
-
-	const handleSubmit = e => {
-		e.preventDefault();
-		console.log(initialValues);
-		localStorage.setItem("token", "tokenValueExample");
-	};
-
+	const { handleSubmit, errors, touched, getFieldProps } = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+			confirmPassword: "",
+		},
+		onSubmit: values => {
+			console.log(values);
+		},
+		validationSchema,
+	});
+	console.log("hola");
 	return (
-		<form className="form-container" onSubmit={handleSubmit}>
-			<input
-				className="input-field"
-				type="text"
-				name="name"
-				value={initialValues.name}
-				onChange={handleChange}
-				placeholder="Enter name"
-			></input>
-			<input
-				className="input-field"
-				type="text"
-				name="lastName"
-				value={initialValues.lastName}
-				onChange={handleChange}
-				placeholder="Enter last name"
-			></input>
-			<button className="submit-btn" type="submit">
-				Register
-			</button>
-		</form>
+		<div className="flex w-full justify-between items-center min-h-screen">
+			<div className="flex flex-col w-1/2 ">
+				<form noValidate className="form-container" onSubmit={handleSubmit}>
+					<div>
+						<h4 className="text-base text-left">Bienvenido</h4>
+						<h1 className="sefl-start text-3xl text-left font-semibold">
+							Inicia sesion con tu cuenta!
+						</h1>
+					</div>
+					<input
+						className="h-14 border rounded-lg p-4"
+						{...getFieldProps("email")}
+						placeholder="Ingresa tu correo electronico"
+					></input>
+					{errors.email && touched.email && (
+						<span className="text-red-400 text-xs">{errors.email}</span>
+					)}
+
+					<input
+						className="h-14 border rounded-lg p-4"
+						{...getFieldProps("password")}
+						placeholder="Ingresa una contraseña nueva"
+					></input>
+					{errors.password && touched.password && (
+						<span className="text-red-400 text-xs">{errors.password}</span>
+					)}
+
+					<input
+						className="h-14 border rounded-lg p-4"
+						{...getFieldProps("confirmPassword")}
+						placeholder="Ingresa nuevamente la contraseña"
+					></input>
+					{errors.confirmPassword && touched.confirmPassword && (
+						<span className="text-red-400 text-xs">
+							{errors.confirmPassword}
+						</span>
+					)}
+
+					<button
+						className="bg-red-500 shadow rounded-lg mt-2 p-2 h-14 text-white font-normal text-2xl"
+						type="submit"
+					>
+						Registrate
+					</button>
+				</form>
+				<div className="absolute bottom-1 left-1/4">
+					<p>
+						Ya tienes cuenta?
+						<a onClick={() => navigate("/login-user", { replace: true })}>
+							Inicia sesión
+						</a>{" "}
+					</p>
+				</div>
+			</div>
+			<div className="w-1/2">
+				<img
+					alt="loginRegister"
+					src="images/image-loginRegister.jpg"
+					className="max-h-screen w-full"
+				/>
+			</div>
+		</div>
 	);
 };
 
