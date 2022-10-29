605ad808c6a869409b3135d4b84f0955422cd758
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-10-29 13:02:49 -0300

    Armado del formulario y redireccion con router
    Validaciones de los campos, integracion dentro del componente, etc.

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App () {
           <Route path="/backoffice/create-slide" element={<SlidesForm/>} />
           <Route path="/create-testimonials" element={<TestimonialForm/>} />
           <Route path="/create-user" element={<UserForm/>} />
-          <Route path="/create-member" element={<MembersForm/>} />
+          <Route path="/backoffice/members/edit" element={<MembersForm/>} />
           <Route path="/create-project" element={<ProjectsForm/>} />
           <Route path="/school-campaign" element={<SchoolCampaign/>} />
           <Route path="/toys-campaign" element={<ToysCampaign/>} />
--- src/Components/Members/MembersForm.jsx
+++ src/Components/Members/MembersForm.jsx
@@ -1,32 +1,105 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React from "react";
+
+import * as yup from "yup"
+import { useFormik } from "formik";
+import  Form  from "../common/Form/Form";
+import FormTitle from "../common/Form/FormTitle"
+import InputImage from "../common/Form/InputImage"
+import FormError from "../common/Form/FormError"
+import FormSubtitle from "../common/Form/FormSubtitle"
+import FormInputText from "../common/Form/FormInputText"
+import FormSubmitButton from "../common/Form/FormSubmitButton"
 
 const MembersForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    name: '',
-    description: ''
-  })
+	const initialValues = {
+		name: "",
+		image: "",
+		facebookUrl: "",
+		linkedinUrl: "",
+		description: "",
+	};
 
-  const handleChange = (e) => {
-    if(e.target.name === 'name'){
-      setInitialValues({...initialValues, name: e.target.value})
-    } if(e.target.name === 'description'){
-      setInitialValues({...initialValues, description: e.target.value})
-    }
-  }
+	const regexURL =
+		"/^((ftp|http|https)://)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(.[a-zA-Z]+)+((/)[w#]+)*(/w+?[a-zA-Z0-9_]+=w+(&[a-zA-Z0-9_]+=w+)*)?$/gm";
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
-  }
+	const validationSchema = yup.object().shape({
+		name: yup
+			.string()
+			.min(4, "Minimo 4 caracteres")
+			.required("Nombre obligatorio"),
+		image: yup.string().required("Imagen obligatoria"),
+		description: yup.string().required("Descripcion obligatoria"),
+		facebookUrl: yup
+			.string()
+			.required("Link obligatorio")
+			.matches(regexURL, "URL invalido"),
+		linkedinUrl: yup
+			.string()
+			.required("Link obligatorio")
+			.matches(regexURL, "URL invalido"),
+	});
 
-  return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name"></input>
-      <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-      <button className="submit-btn" type="submit">Send</button>
-    </form>
-  );
-}
- 
+	const onSubmit = () => {
+		console.log(values);
+	};
+
+	const formik = useFormik({
+		initialValues,
+		validationSchema,
+		onSubmit,
+	});
+	const {
+		handleSubmit,
+		errors,
+		handleChange,
+		handleBlur,
+		values,
+		touched,
+		setFieldValue,
+	} = formik;
+
+	return (
+		<Form handleSubmit={handleSubmit}>
+			<FormTitle>Crear/Editar Miembro</FormTitle>
+			<InputImage
+				bgImage={values.image}
+				formikFieldName="image"
+				setFieldValue={setFieldValue}
+			/>
+			<FormError error={errors.image} touched={touched.image} />
+			<FormSubtitle>Nombre</FormSubtitle>
+			<FormInputText
+				type="text"
+				name="name"
+				valueToShow={values.name}
+				handleChange={handleChange}
+				handleBlur={handleBlur}
+				placeholder="Juan Perez"
+			/>
+			<FormError error={errors.name} touched={touched.name} />
+			<FormSubtitle>Facebook URL</FormSubtitle>
+			<FormInputText
+				type="text"
+				name="facebookUrl"
+				valueToShow={values.facebookUrl}
+				handleChange={handleChange}
+				handleBlur={handleBlur}
+				placeholder="www.facebook.com/JuanPerez"
+			/>
+			<FormError error={errors.facebookUrl} touched={touched.facebookUrl} />
+			<FormSubtitle>Linkedin URL</FormSubtitle>
+			<FormInputText
+				type="text"
+				name="linkedinUrl"
+				valueToShow={values.linkedinUrl}
+				handleChange={handleChange}
+				handleBlur={handleBlur}
+				placeholder="www.linkedin.com/JuanPerez"
+			/>
+			<FormError error={errors.linkedinUrl} touched={touched.linkedinUrl} />
+			<FormSubmitButton>Enviar</FormSubmitButton>
+		</Form>
+	);
+};
+
 export default MembersForm;
