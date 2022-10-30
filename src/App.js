09c1746e1c02dadb106e4416d20f4b9c8ff01f9b
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-10-29 21:31:39 -0300

    Estilos a Novedades

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import NewsDetails from "./Components/News/Details/NewsDetails";
 function App() {
 	const newsDetail = {
 		name: "Prueba novedades (っ ◕‿◕ )っ",
-		image: "http://ongapi.alkemy.org/storage/7wu0eUuXSa.png",
+		image: "https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE5fHx8ZW58MHx8fHw%3D&w=1000&q=80",
 		content:
 			"Shred all toilet paper and spread around the house scratch the postman wake up lick paw wake up owner meow meow and go into a room to decide you didn't want to be in there anyway cats go for world domination. Relentlessly pursues moth. When owners are asleep, cry for no apparent reason catch eat throw up catch eat throw up bad birds. Sleep nap iâ€™m so hungry iâ€™m so hungry but ew not for that plan your travel yet it's 3am, time to create some chaos thinking about you i'm joking it's food always food, whatever yet do not try to mix old food with new one to fool me!. Mew stare at wall turn and meow stare at wall some more meow again",
 	};
--- src/Components/News/Details/NewsDetails.jsx
+++ src/Components/News/Details/NewsDetails.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const NewsDetails = ({ details }) => {
 	return (
-		<div className="w-screen h-screen">
-			<h1 className="text-3xl font-bold text-center">Novedades</h1>
-			<img src={details.image} />
+		<div className="w-screen h-screen flex flex-col gap-7 items-center">
+			<h1 className="text-4xl font-bold text-center pt-7">Novedades</h1>
 			<div
-				className={`w-full h-96 bg-center bg-auto bg-[url('${details.image}')]`}
+				className="w-full h-96 bg-center bg-cover bg-no-repeat"
+				style={{ backgroundImage: `url("${details.image}")` }}
 			/>
-			<h2>{details.name}</h2>
-			<div dangerouslySetInnerHTML={{ __html: details.content }} />
+			<div className="w-11/12 sm:w-3/4 md:w-2/4 lg:w-2/5">
+				<h2 className="text-2xl font-bold">{details.name}</h2>
+				<div className="font-light" dangerouslySetInnerHTML={{ __html: details.content }} />
+			</div>
 		</div>
 	);
 };
