5ceffa98bce4abf694dbf0ea5524389163d0e1a6
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-10-31 13:48:00 -0300

    Creacion de componente Layout

---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Este es un footer xd
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        Esto es un header xd
+    </div>
+  )
+}
+
+export default Header
