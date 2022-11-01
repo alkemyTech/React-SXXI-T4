ad1500b34b380bb8564610327f48b3ef022776a3
Eduardo Gabriel Pindo <69809704+gabrielpindo@users.noreply.github.com>
2022-11-01 09:39:55 -0300

    Add files via upload

---
+++ src/Components/Title/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const Test = () => {
+  return (
+    <div>
+        <Title text="¡Este es un título de prueba!" background="/images/sideshow-img.jpg"/>
+    </div>
+  )
+}
+
+export default Test
