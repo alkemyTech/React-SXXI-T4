b526db6a2a553cfc3c18688bd51fb64dbff80284
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-11-02 17:39:46 -0300

    Espacios inecesarios

--- src/Components/common/Form/Form.jsx
+++ src/Components/common/Form/Form.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Form = ({ children, handleSubmit }) => {
 	return (
 		<form
-			className="  flex flex-col justify-center align-middle w-11/12 md:w-3/4 lg:w-2/4 h-screen gap-3 mx-auto"
+			className="flex flex-col justify-center align-middle w-11/12 md:w-3/4 lg:w-2/4 h-screen gap-3 mx-auto"
 			onSubmit={handleSubmit}
 		>
 			{children}
