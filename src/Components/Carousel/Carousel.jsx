1b668fcf6f05dbc21a3fefeee2e79a83d5f56533
Shonakar <carlosjbordon@gmail.com>
2022-11-03 01:57:05 -0300

    handlers, automatic slides, clean css

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -4,6 +4,12 @@ import "./Carousel.css";
 
 const Carousel = () => {
 	const [slides, setSlides] = useState([]);
+	const [sliderPosition, setSliderPosition] = useState(0);
+
+	const cleanSlidesArray = array => {
+		const cleanArray = array.filter(item => item.image !== null);
+		return cleanArray;
+	};
 
 	const getSlides = async () => {
 		const res = { data: {}, error: null };
@@ -13,22 +19,57 @@ const Carousel = () => {
 		} catch (error) {
 			res.error = error.message;
 		}
-		setSlides(res.data);
+
+		setSlides(cleanSlidesArray(res.data));
 	};
 
 	useEffect(() => {
 		getSlides();
 	}, []);
 
+	useEffect(() => {
+		setTimeout(() => {
+			handlerNext();
+		}, 5000);
+	});
+
+	const handlerPrevious = () => {
+		let newPos = sliderPosition;
+		if (newPos > 0) {
+			newPos = newPos - 1;
+		}
+		setSliderPosition(newPos);
+		translateSlides(newPos);
+	};
+
+	const handlerNext = () => {
+		let newPos = sliderPosition;
+		if (newPos < slides.length - 1) {
+			newPos = newPos + 1;
+		} else if (sliderPosition === slides.length - 1) {
+			newPos = 0;
+		}
+
+		setSliderPosition(newPos);
+		translateSlides(newPos);
+	};
+
+	const translateSlides = newPos => {
+		const toTranslate = -100 * newPos;
+		for (let i = 0; i < slides.length; i++) {
+			const elem = document.getElementById(`item${i}`);
+			elem.style.transform = `translateX(${toTranslate}%)`;
+		}
+	};
 	return (
 		<div>
 			<div className="container">
-				<div>
+				<div className="leftArrow" onClick={handlerPrevious}>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
 						fill="currentColor"
-						className="leftArrow w-6 h-6"
+						className="w-6 h-6"
 					>
 						<path
 							fillRule="evenodd"
@@ -37,25 +78,33 @@ const Carousel = () => {
 						/>
 					</svg>
 				</div>
-				<div className="Display-Frame">
-					{slides.map(item => (
-						<div className="item" key={item.id}>
+				<div className="displayFrame">
+					{slides.map((item, index) => (
+						<div className="item" id={`item${index}`} key={item.id}>
 							<img src={`${item.image}`} alt={item.name} />
+							<div className="text">
+								<div className="font-semibold">{item.name}</div>
+								<div
+									dangerouslySetInnerHTML={{ __html: item.description }}
+								></div>
+							</div>
 						</div>
 					))}
 				</div>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					className="rightArrow w-6 h-6"
-				>
-					<path
-						fillRule="evenodd"
-						d="M4.72 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 010-1.06zm6 0a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 010-1.06z"
-						clipRule="evenodd"
-					/>
-				</svg>
+				<div className=" rightArrow" onClick={handlerNext}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							fillRule="evenodd"
+							d="M4.72 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 010-1.06zm6 0a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 010-1.06z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</div>
 			</div>
 		</div>
 	);
