a391ed5f5a5c305698e840bdb42d85452829a542
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2022-11-04 14:12:11 -0300

    update LoginForm.js

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -6,41 +6,43 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const LoginForm = () => {
-	const message = "Por favor, completa los campos";
-	const messageEmail = "Ingrese un correo electrónico válido"
-	const messagePassword = "La contraseña debe tener una longitud mínima de 6 caraceteres, y contener al menos un número, una letra y un símbolo"
 	const [loginData, setLoginData] = useState("");
-	
+
+	const messages = {
+		messageRgx:
+			/^.*(?=.{6,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1}).*$/,
+		messageRequired: "Este campo es requerido",
+		messageEmail: "Debe contener un correo electronico valido.",
+		messagePassRequired:
+			"La contraseña debe contener al enos 6 caracteres, una letra, un numero y un simbolo $%#",
+	};
+
 	return (
 		<div className="flex w-full bg-slate-100 justify-between items-center min-h-screen">
 			<div className="w-full sm:w-1/2 md:w-1/2 flex flex-col justify-center items-center loginForm">
 				<Formik
 					initialValues={{ email: "", password: "" }}
-					onSubmit={(values, {resetForm}) => {
+					onSubmit={(values, { resetForm }) => {
 						setLoginData(values);
 						Swal.fire({
-							position: 'top-end',
-							icon: 'success',
-							title: '¡Bienvenido!' ,
-							text: `${loginData.email}`
-						  })
-						  
-						  
-						  
+							position: "top-end",
+							icon: "success",
+							title: "¡Bienvenido!",
+							text: `${loginData.email}`,
+						});
+
+						resetForm(values);
 					}}
 					validationSchema={() =>
 						yup.object().shape({
 							email: yup
 								.string()
-								.email(messageEmail)
-								.required(message),
+								.email(messages.messageEmail)
+								.required(messages.messageRequired),
 							password: yup
 								.string()
-								.matches(
-									/^.*(?=.{6,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1}).*$/,
-									messagePassword
-								)
-								.required(message),
+								.matches(messages.messageRgx, messages.messagePassRequired)
+								.required(messages.messageRequired),
 						})
 					}
 				>
@@ -50,8 +52,10 @@ const LoginForm = () => {
 								<div className="mx-auto md:hidden">
 									<img src="/images/LOGO-SOMOSMAS.png" />
 								</div>
-								<div className="">
-									<h4 className="md:text-left text-center font-bold md:font-normal sm:font-semi-bold pb-10 md:pb-0">Bienvenido</h4>
+								<div>
+									<h4 className="md:text-left text-center text-2xl font-bold md:font-normal sm:font-semi-bold pb-5 md:pb-0">
+										Bienvenido
+									</h4>
 									<h1 className="sefl-start text-2xl md:text-3xl sm:text-left text-center font-semibold hidden md:block">
 										¡Inicia sesión en tu cuenta!
 									</h1>
@@ -65,7 +69,6 @@ const LoginForm = () => {
 									placeholder="Ingresa tu correo electrónico"
 								/>
 								<ErrorMessage
-									
 									name="email"
 									component={() => (
 										<span className="text-red-400 text-xs">{errors.email}</span>
@@ -102,20 +105,19 @@ const LoginForm = () => {
 					)}
 				</Formik>
 				<div className="absolute bottom-4 flex gap-2">
-					<p className="font-medium text-slate-600">
-						¿No tienes una cuenta?
-					</p>
-					<Link
-						className="text-red-600 font-medium"
-						to="/register-user"
-					>
+					<p className="font-medium text-slate-600">¿No tienes una cuenta?</p>
+					<Link className="text-red-600 font-medium" to="/register-user">
 						Registrar
 					</Link>
 				</div>
 			</div>
 
 			<div className="loginImg">
-				<img alt="loginForm" src="/images/juntosImg.jpg" className="max-h-screen w-full" />
+				<img
+					alt="loginForm"
+					src="/images/juntosImg.jpg"
+					className="max-h-screen w-full"
+				/>
 			</div>
 		</div>
 	);
