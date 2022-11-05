78ff8343a1c9f2b84bdf5ca5276441567e983f17
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-11-05 14:19:38 -0300

    Creado de componente Contenido de actividad

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import ActivitiesForm from './Components/Activities/ActivitiesForm'
-import CategoriesForm from './Components/Categories/CategoriesForm'
-import NewsForm from './Components/News/NewsForm'
-import SlidesForm from './Components/Slides/SlidesForm'
-import TestimonialForm from './Components/Testimonials/TestimonialsForm'
-import UserForm from './Components/Users/UsersForm'
-import SchoolCampaign from './Campaigns/School/SchoolCampaign'
-import ToysCampaign from './Campaigns/Toys/ToysCampaign'
-import MembersForm from './Components/Members/MembersForm'
-import ProjectsForm from './Components/Projects/ProjectsForm'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
+import ContentActivities from "./Components/Activities/ContentActivities";
 
-function App () {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" element={<ActivitiesForm/>} />
-          <Route path="/create-category" element={<CategoriesForm/>} />
-          <Route path="/create-news" element={<NewsForm/>} />
-          <Route path="/backoffice/create-slide" element={<SlidesForm/>} />
-          <Route path="/create-testimonials" element={<TestimonialForm/>} />
-          <Route path="/create-user" element={<UserForm/>} />
-          <Route path="/create-member" element={<MembersForm/>} />
-          <Route path="/create-project" element={<ProjectsForm/>} />
-          <Route path="/school-campaign" element={<SchoolCampaign/>} />
-          <Route path="/toys-campaign" element={<ToysCampaign/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+			<ContentActivities content={"<p>Buenas</p>"}/>
+				<Routes>
+					<Route path="/create-activity" element={<ActivitiesForm />} />
+					<Route path="/create-category" element={<CategoriesForm />} />
+					<Route path="/create-news" element={<NewsForm />} />
+					<Route path="/backoffice/create-slide" element={<SlidesForm />} />
+					<Route path="/create-testimonials" element={<TestimonialForm />} />
+					<Route path="/create-user" element={<UserForm />} />
+					<Route path="/create-member" element={<MembersForm />} />
+					<Route path="/create-project" element={<ProjectsForm />} />
+					<Route path="/school-campaign" element={<SchoolCampaign />} />
+					<Route path="/toys-campaign" element={<ToysCampaign />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
