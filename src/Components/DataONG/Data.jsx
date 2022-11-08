eb8a29c6567e9799639c55be869bb1615719bc5a
Gabriel <gabrielpindo@gmail.com>
2022-11-08 18:42:20 -0300

    Correcciones realizadas componente de Screen de datos de la organización.
    Texto, imagen y título tomados desde la API.

--- src/Components/DataONG/Data.jsx
+++ src/Components/DataONG/Data.jsx
@@ -1,49 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import Title from '../Title/Title'
 import './Data.css'
 
 const Data = () => {
+
+    const [data, setData] = useState({})
+    useEffect(() => {
+        axios.get("https://ongapi.alkemy.org/api/organization/4")
+            .then(data => setData(data.data.data))
+            .catch(error => console.log(error))
+    })
+
+
     return (
         <div className="dataOrganization">
-            <h1 id="ONGTitle">ONG</h1>
-            <h1>SOMOS MÁS</h1>
+            <Title 
+                text={data.name}     
+                background="/images/staff-picks.jpg"    
+            />
+
             <div className="dataOrganizationContent">
 
                 <div className='dataImg'>
-                    <img src="/images/Foto 11.jpg" alt="" />
+                    <img src={data.logo} alt="" />
                 </div>
 
                 <div className="dataText">
-                    <h3>¿Quiénes somos?</h3>
-                    <p>Somos una ONG que tiene como principal objetivo promover la inclusión social de los sectores más vulnerables de la Argentina.
-                        El trabajo es realizado por un gran número de voluntarios mediante un abordaje integral que incluye la asistencia, la contención, la capacitación, la educación y la cultura del trabajo.
-                        Quienes formamos Sí creemos en el trabajo de igual a igual, en el trabajo en equipo, en la heterogeneidad de los grupos, en la complementación y en las nuevas generaciones.
-                        Quienes formamos Sí estamos convencidos de que transformar la realidad es posible y, para lograrlo, necesitamos el compromiso de todos.
-                    </p>
-                </div>
-            </div>
-
-
-            <div className="dataOrganizationContent dataOrg2">
-
-                <div className="dataText">
-                    <h3>Accesibilidad</h3>
                     <p>
-                        Nuestro objetivo es que sea accesible para todas las personas sin distinguir su edad, sus capacidades, su conocimiento de Internet, la velocidad de su conexión ni el dispositivo que utiliza para navegar (computadora, tableta, celular económico o smartphone). Para ello, nos manejamos con los Estándares Web actuales.
-                        Además, queremos que sea compatible con la mayor cantidad de navegadores de Internet (varias versiones de Explorer, Chrome, Firefox, Safari, Edge, entre otros) y con las herramientas que utilizan las personas con disminución visual.
-                        Escribimos los textos de modo claro para una fácil comprensión, sin palabras extrañas o innecesariamente difíciles.
-                        Si tenés algún problema para encontrar información o navegar en el sitio, ves algún error o te parece que algo no está bien explicado, avisanos para que podamos corregirlo. Queremos que Argentina.gob.ar sea fácil de usar y te resulte una herramienta útil.
+                        {data.short_description}
                     </p>
                 </div>
-                <div className='dataImg'>
-                    <img src="/images/Foto10.jpg" alt="Imagen no encontrada" />
-                </div>
             </div>
+
 
 
             <div className="dataOrganizationEdit ">
                 <h3>¿Deseas editar la información de tu ONG?</h3>
-                <Link to="/register-user" className="link">Haz click aquí</Link> 
+                <Link to="/backoffice/organization/edit" className="link">Haz click aquí</Link>
             </div>
         </div>
     )
