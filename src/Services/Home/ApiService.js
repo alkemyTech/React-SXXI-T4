58c0149adf216877c28e7c0aa176fc331d59e6ea
Gabriel <gabrielpindo@gmail.com>
2022-11-11 00:06:57 -0300

    Edición de Inicio, formulario

---
+++ src/Services/Home/ApiService.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+
+export const getWelcomeText = (setData) => {
+	axios
+		.get(`https://ongapi.alkemy.org/api/organization`)
+		.then(res => {
+			setData(res.data)
+		})
+		.catch(error => console.log(error));
+};
+
+export const putWelcomeText = value => {
+	axios
+		.put(`https://ongapi.alkemy.org/api/organization/4`, value)
+		.then(res => console.log(res))
+		.catch(err => console.log(err));
+};
+
+
+
+export const getSlides = setData => {
+	axios
+		.get(`https://ongapi.alkemy.org/api/slides`)
+		.then(res => {
+			setData(res.data.data.slice(-3));
+		})
+		.catch(error => console.log(error));
+};
+
+export const putSlides = value => {
+	console.log(value.id);
+	axios
+		.put(`https://ongapi.alkemy.org/api/slides/${value.id}`, value)
+		.then(res => console.log(res))
+		.catch(err => console.log(err));
+};
