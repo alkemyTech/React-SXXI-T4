c70175003b797ea6af48fbc13fc19001ec2e8fa6
Gabriel <gabrielpindo@gmail.com>
2022-11-14 00:07:29 -0300

    Update

---
+++ src/Components/common/Form/FormTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormTitle = ({children}) => {
+  return (
+    <h2 className="text-2xl text-center font-bold text-gray-500">
+        {children}
+    </h2>
+  )
+}
+
+export default FormTitle
