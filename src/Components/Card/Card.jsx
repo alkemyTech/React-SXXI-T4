be5908354d00b046d6c1122ad37f9f8a730f3547
Gabriel <gabrielpindo@gmail.com>
2022-11-14 08:27:29 -0300

    Update de la parte de "Nosotros" (Staff), con la <Card/> aprobada.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Card.css'
 
 const Card = (data) => {
-
+    
     const divStyle = {
         backgroundColor: `${data.color}`
     };
@@ -12,7 +12,11 @@ const Card = (data) => {
 
             <div className="cardContainer">
                 <div className="cardImage">
-                    <img src={`${data.image}`} alt="img no disponible" />
+                    <img src={`${data.image}`} onError={
+                        (e) => {
+                            e.currentTarget.src = "https://images-ext-2.discordapp.net/external/6ulAMx1V10CIaXeq3tX1eHslMwU8yBZl2AGZ4RllfZ4/https/img.icons8.com/material-outlined/512/add-image.png?width=490&height=490"
+                        }
+                    } />
                 </div>
 
                 <div className="cardText">
@@ -23,9 +27,13 @@ const Card = (data) => {
                         <p>{data.description}</p>
                     </div>
                 </div>
-                {data.socialMedia === true ?
+                {((data.facebook === "" || data.facebook === undefined || data.facebook === null || data.linkedin === "" || data.linkedin === null || data.linkedin === undefined)) ?
+
                     <div style={divStyle} className="cardMedia">
-                        {data.mode === "dark"
+                    </div>
+                    :
+                    <div style={divStyle} className="cardMedia">
+                        {data.iconMode === "dark"
 
                             ?
                             <div style={divStyle} className="cardMedia">
@@ -48,11 +56,7 @@ const Card = (data) => {
                         }
                     </div>
 
-                    :
-                    <div style={divStyle} className="cardMedia">
-                        
-                    </div>
-                    }
+                }
             </div>
 
 
