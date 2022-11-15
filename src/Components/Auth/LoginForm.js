418f0f1f2e92916ec111e87c16ad24699d2987d0
Gabriel <gabrielpindo@gmail.com>
2022-11-15 01:25:25 -0300

    update

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -9,72 +9,90 @@ import FormError from "../common/Form/FormError";
 import FormInputPassword from "../common/Form/FormInputPassword";
 import Form from "../common/Form/Form";
 import FormTitle from "../common/Form/FormTitle";
-import FormSubmitButton from "../common/Form/FormSubmitButton";
-import {yupErrorMessages,yupRegexValidation} from "../../utils/messages/formMessagesValidation"
+import FormContainerInput from "../common/Form/FormContainerInput";
+import {
+	yupErrorMessages,
+	yupRegexValidation,
+} from "../../utils/messages/formMessagesValidation";
+import LayoutForm from "../Layout/LayoutForm/LayoutForm";
 
 const LoginForm = () => {
 	const [loginData, setLoginData] = useState("");
 
 	return (
-		<div className="flex w-full bg-slate-100 justify-between items-center min-h-screen">
-			<div className="w-full sm:w-1/2 md:w-1/2 flex flex-col justify-center items-center loginForm">
-				<Formik
-					initialValues={{ email: "", password: "" }}
-					onSubmit={(values, { resetForm }) => {
-						setLoginData(values);
-						Swal.fire({
-							position: "top-end",
-							icon: "success",
-							title: "¡Bienvenido!",
-							text: `${loginData.email}`,
-						});
+		<div className="w-100 flex">
+			<div className="basis-1/2">
+				<LayoutForm>
+					<Formik
+						initialValues={{ email: "", password: "" }}
+						onSubmit={(values, { resetForm }) => {
+							setLoginData(values);
+							Swal.fire({
+								position: "top-end",
+								icon: "success",
+								title: "¡Bienvenido!",
+								text: `${loginData.email}`,
+							});
 
-						resetForm(values);
-					}}
-					validationSchema={() =>
-						yup.object().shape({
-							email: yup
-								.string()
-								.email(yupErrorMessages.invalidEmail)
-								.required(yupErrorMessages.required),
-							password: yup
-								.string()
-								.matches(yupRegexValidation, yupErrorMessages.passRequired)
-								.required(yupErrorMessages.required),
-						})
-					}
-				>
-					{({ errors, touched }) => (
-						<Form>
-							<div className="w-full flex flex-col  gap-4 welcomeLogin">
-								<div className="mx-auto md:hidden">
-									<img src="/images/LOGO-SOMOSMAS.png" />
+							resetForm(values);
+						}}
+						validationSchema={() =>
+							yup.object().shape({
+								email: yup
+									.string()
+									.email(yupErrorMessages.invalidEmail)
+									.required(yupErrorMessages.required),
+								password: yup
+									.string()
+									.matches(yupRegexValidation, yupErrorMessages.required)
+									.required(yupErrorMessages.required)
+							})
+						}
+					>
+						{({ errors, touched }) => (
+							<Form>
+								<div className="place-content-center grid justify-items-center">
+									<img
+										className="place-content-center grid justify-items-center"
+										src="/images/LOGO-SOMOSMAS.png"
+									/>
 								</div>
-								<div>
-									<FormTitle>Bienvenido</FormTitle>
-									<FormTitle>¡Inicia sesión en tu cuenta!</FormTitle>
+								<FormTitle>Bienvenido</FormTitle>
+								<FormTitle>¡Inicia sesión en tu cuenta!</FormTitle>
+
+								<FormContainerInput>
+									<FormInputText
+										type="text"
+										name="email"
+										placeholder="Ingresa tu correo electrónico"
+									/>
+									<FormError error={errors.email} touched={touched.email} />
+
+									<FormInputPassword
+										name="password"
+										placeholder="Ingresa tu contraseña"
+									/>
+									<FormError
+										error={errors.password}
+										touched={touched.password}
+									/>
+								</FormContainerInput>
+
+								<div className="w-100 justify-center items-center flex m-10">
+									<button
+										type="submit"
+										className="w-1/3  py-2 bg-sky-600   tracking-wide 
+									rounded-lg  hover:bg-sky-500 hover:-translate-y-1 
+									transition-all duration-500 text-white text-lg font-medium"
+									>
+										Iniciar sesión
+									</button>
 								</div>
-
-								<FormInputText
-									type="text"
-									name="email"
-									placeholder="Ingresa tu correo electrónico"
-								/>
-								<FormError error={errors.email} touched={touched.email}  />
-
-								<FormInputPassword
-									name="password"
-									placeholder="Ingresa tu contraseña"
-									
-								/>
-								<FormError error={errors.password} touched={touched.password} />
-
-								<FormSubmitButton type="submit">Inicia sesión</FormSubmitButton>
-							</div>
-						</Form>
-					)}
-				</Formik>
-				<div className="absolute bottom-4 flex gap-2">
+							</Form>
+						)}
+					</Formik>
+				</LayoutForm>
+				<div className="absolute bottom-4 flex gap-4">
 					<p className="font-medium text-slate-600">¿No tienes una cuenta?</p>
 					<Link className="text-red-600 font-medium" to="/register-user">
 						Registrar
@@ -82,7 +100,7 @@ const LoginForm = () => {
 				</div>
 			</div>
 
-			<div className="loginImg">
+			<div className="loginImg basis-1/2 ">
 				<img
 					alt="loginForm"
 					src="/images/juntosImg.jpg"
--- src/Components/common/Form/FormSubmitButton.jsx
+++ src/Components/common/Form/FormSubmitButton.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const FormSubmitButton = () => {
 	return (
-		<div className="absolute w-1/5 bottom-1 right-5">
+		<div className="w-100 justify-center items-center flex m-10">
 			<button
 				type="submit"
-				className="w-full  py-2 bg-sky-600   tracking-wide 
+				className="w-1/3  py-2 bg-sky-600   tracking-wide 
 									rounded-lg  hover:bg-sky-500 hover:-translate-y-1 
 									transition-all duration-500 text-white text-lg font-medium"
 			>
-				Enviar
+				Iniciar sesión
 			</button>
 		</div>
 	);
