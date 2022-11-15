d81e6cd191dde6fc58637b13c1b94ff43148e92f
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2022-11-14 23:53:41 -0300

    update de componente novedad

--- src/App.js
+++ src/App.js
@@ -12,48 +12,13 @@ import MembersForm from "./Components/Members/MembersForm";
 import ProjectsForm from "./Components/Projects/ProjectsForm";
 import News from "./Components/News";
 function App() {
-	const data = [
-		{
-			id: 1,
-			name: "Novedad 1",
-			fecha: "7/11/2022",
-			content:
-				"Es una descripcion de la novedad numero 1 donde se muestra informacion actualizada de lo ultimo de las novedades",
-			image: "Foto6.jpg",
-		},
-		{
-			id: 2,
-			name: "Novedad 2",
-			fecha: "6/11/2022",
-			content:
-				"Es una descripcion de la novedad numero 2 donde se muestra informacion actualizada de lo ultimo de las novedades",
-			image: "Foto10.jpg",
-		},
-		{
-			id: 3,
-			name: "Novedad 3",
-			fecha: "5/11/2022",
-			content:
-				"Es una descripcion de la novedad numero 3 donde se muestra informacion actualizada de lo ultimo de las novedades",
-			image: "Foto11.jpg",
-		},
-		{
-			id: 4,
-			name: "Novedad 4",
-			fecha: "4/11/2022",
-			content:
-				"Es una descripcion de la novedad numero 4 donde se muestra informacion actualizada de lo ultimo de las novedades",
-			image: "Manos10.jpg",
-		},
-	];
-
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/create-activity" element={<ActivitiesForm />} />
 					<Route path="/create-category" element={<CategoriesForm />} />
-					<Route path="/novedades" element={<News data={data} />} />
+					<Route path="/novedades" element={<News />} />
 					<Route path="/create-news" element={<NewsForm />} />
 					<Route path="/backoffice/create-slide" element={<SlidesForm />} />
 					<Route path="/create-testimonials" element={<TestimonialForm />} />
