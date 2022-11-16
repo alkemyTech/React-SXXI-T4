cfb89721d42546b33ec9ae26d58b70b83b508b61
Shonakar <carlosjbordon@gmail.com>
2022-11-16 10:48:52 -0300

    update convertBase64

--- src/utils/ConvertBase64/ConvertBase64.js
+++ src/utils/ConvertBase64/ConvertBase64.js
@@ -1,22 +1,19 @@
 import Swal from "sweetalert2";
-
-export const convertBase64 = (setFieldvalue, inputImage) => {
-	const file = inputImage.current.files[0];
+export const convertBase64 = (setFieldValue, FieldName, value) => {
 	const reader = new FileReader();
 	const extensions = /(jpe?g|png)$/i;
-
-	if (!extensions.test(file.type)) {
+	if (!extensions.test(value.files[0].type)) {
 		Swal.fire({
 			icon: "error",
-			title: "¡Formato no valido!",
+			title: "¡Formato no válido!",
 			text: "Seleccione un formato .png o .jpg.",
 		});
 		return;
 	}
 
-	reader.readAsDataURL(inputImage.current.files[0]);
 	reader.onload = () => {
-		const base64 = reader.result;
-		setFieldvalue("logo", base64);
+		const codeBase64 = reader.result;
+		setFieldValue(FieldName, codeBase64);
 	};
+	reader.readAsDataURL(value.files[0]);
 };
