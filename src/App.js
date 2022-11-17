27f8300608a8a7695f39497ac79173cb28eebec7
Shonakar <carlosjbordon@gmail.com>
2022-11-17 01:22:45 -0300

    slides form

--- src/App.js
+++ src/App.js
@@ -39,7 +39,8 @@ function App() {
 						<Route path="/register-user" element={<RegisterForm />} />
 						<Route path="/create-contact" element={<ContactForm />} />
 						<Route path="/login-user" element={<LoginForm />} />
-						<Route path="/backoffice/create-slide" element={<SlidesForm />} />
+						<Route path="/backoffice/slides" element={<SlidesForm />} />
+						<Route path="/backoffice/slides/:id" element={<SlidesForm />} />
 						<Route path="/create-testimonials" element={<TestimonialForm />} />
 						<Route path="/create-member" element={<MembersForm />} />
 						<Route path="/create-project" element={<ProjectsForm />} />
