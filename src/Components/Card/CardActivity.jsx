5381aa8c68747d5806492f8f9c8b1ca0916ba671
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-11-17 16:47:05 -0300

    Estilos a imagen(objecto fit) e imagen no encontrada

--- src/Components/Card/CardActivity.jsx
+++ src/Components/Card/CardActivity.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
+import imageNotFound from "images/imageNotFound.png";
+
 const CardActivity = ({ activity }) => {
 	return (
 		<div className="bg-white w-full h-auto rounded relative">
 			<img
-				src={`/images/${activity.image}`}
-				className="rounded w-auto h-auto"
+				src={`${activity.image || imageNotFound}`}
+				className="rounded w-full h-48 object-cover object-center"
 			/>
 			<div className="p-2">
 				<h1 className="font-semibold text-lg tracking-wide">{activity.name}</h1>
 				<p className="py-2 font-normal text-sm text-slate-400">
-					{activity.content}
+					{activity.description}
 				</p>
 				<div className="py-3">
 					<h5 className="absolute text-sm text-slate-400 bottom-0 right-2">
