d9380d4c07fad677155ec088d86ebed7c245427f
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-11-17 17:07:22 -0300

    Correccion de titulo

--- src/Components/Activities/ActivitiesList.jsx
+++ src/Components/Activities/ActivitiesList.jsx
@@ -17,7 +17,7 @@ const ActivitiesList = () => {
 	return (
 		<div className="flex justify-center p-5">
 			<div className="bg-slate-100 w-full sm:w-full md:w-4/5 lg:w-4/5 shadow-xl rounded ">
-				<Title text="Novedades" />
+				<Title text="Actividades" />
 				<div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3">
 					{activities?.map(activity => (
 						<CardActivity
