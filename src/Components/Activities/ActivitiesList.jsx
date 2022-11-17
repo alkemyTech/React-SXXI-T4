e28654bce238e20397b2d020680427e4f2c7a013
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-11-17 19:32:12 -0300

    Importacion componente generico Card e implementacion

--- src/Components/Activities/ActivitiesList.jsx
+++ src/Components/Activities/ActivitiesList.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { findAll } from "Services/ActivitiesServices";
 
 import Title from "Components/Title/Title";
-import CardActivity from "Components/Card/CardActivity";
+import Card from "Components/Card/Card";
 
 const ActivitiesList = () => {
 	const [activities, setActivities] = useState([]);
@@ -18,11 +18,13 @@ const ActivitiesList = () => {
 		<div className="flex justify-center p-5">
 			<div className="bg-slate-100 w-full sm:w-full md:w-4/5 lg:w-4/5 shadow-xl rounded ">
 				<Title text="Actividades" />
-				<div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3">
+				<div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 place-items-center">
 					{activities?.map(activity => (
-						<CardActivity
+						<Card
 							key={"activityCard" + activity.id}
-							activity={activity}
+							title={activity.name}
+							image={activity.image}
+							description={activity.description}
 						/>
 					))}
 				</div>
