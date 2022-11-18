287a69113540149929bb8f05d65ad2f24c329ab5
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2022-11-18 01:59:41 -0300

    pull de feature 123

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ import Backoffice from "./Components/Backoffice/Backoffice";
 import { Home } from "Components/Home";
 import RegisterForm from "Components/Auth/RegisterForm";
 import LoginForm from "Components/Auth/LoginForm";
-import Layout from "Components/Layout/Layout";
 
 function App() {
 	return (
