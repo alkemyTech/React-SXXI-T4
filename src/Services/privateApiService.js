d811cfcb77bef06f589b10f76fe4254dc6144de2
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-11-22 18:15:11 -0300

    Metodo devolver bearer + token

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,4 @@
-import axios from 'axios';
-
-const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
+export const verifyTokenLocalStorage = ()=>{
+    const token = localStorage.getItem("token");
+    return token&&{'Authorization': 'Bearer ' + token}
 }
-
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get
