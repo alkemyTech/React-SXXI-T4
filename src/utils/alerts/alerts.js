e95586c6af0625beb4615fef587704b560742bdb
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2022-11-23 16:46:17 -0300

    Alertas funciones
    Se realizaron las funciones para los distintos tipos de alertas.

--- src/utils/alerts/alerts.js
+++ src/utils/alerts/alerts.js
@@ -3,21 +3,27 @@ import Swal from "sweetalert2";
 export const success = () => {
 	Swal.fire({
 		icon: "success",
-		text: "Se creó con exito!",
-
+		text: "¡Creación exitosa!",
 	});
 };
 
 export const error = () => {
 	Swal.fire({
 		icon: "error",
-		text: "¡Se ha encontrado un error!",
+		text: "¡Hemos encontrado un error!",
 	});
 };
 
 export const update = () => {
 	Swal.fire({
 		icon: "success",
-		text: "Se actualizó con éxito!",
+		text: "¡Se actualizó con éxito!",
+	});
+};
+
+export const info = () => {
+	Swal.fire({
+		icon: "success",
+		text: "Su mensaje se envio con exito, pronto nos contactaremos!",
 	});
 };
