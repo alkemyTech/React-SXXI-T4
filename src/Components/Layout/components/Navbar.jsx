52826b1d2e8c28e1803503ee26981486fc26acec
Gabriel <gabrielpindo@gmail.com>
2022-11-22 23:57:54 -0300

    update nav

--- src/Components/Layout/components/Navbar.jsx
+++ src/Components/Layout/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import { useParams, Link } from 'react-router-dom';
 const Navbar = () => {
     const id = { useParams }
+    const [mobile, setMobile] = useState(false)
 
     const menuArr = [
         {
@@ -39,15 +39,14 @@ const Navbar = () => {
 
 
     return (
-        <div className="w-full sticky top-0 z-50 h-[80px] flex align-middle ">
-            <nav className="w-full  flex justify-between pl-5 pr-5 align-middle shadow-lg shadow-slate-200">
-                <img src="images/assets/LOGO-SOMOSMAS.png" onError={
+        <div>
+            <nav className="hidden lg:w-full bg-white sticky top-0 z-50 h-[80px] md:flex justify-between pl-5 pr-5 align-middle shadow-lg shadow-slate-200">
+                <img src="images/assets/LOGO-SOMOSMAS.png" className="w-full md:w-1/4 object-contain " onError={
                     (e) => {
                         e.currentTarget.src = "images/assets/404.png"
                     }
                 } />
-
-                <ul className="flex justify-around text-sm invisible  align-middle w-full md:visible md:text-sm lg:w-3/4 xl:w-1/2">
+                <ul className="md:flex justify-around text-sm hidden align-middle w-full md:text-sm lg:w-3/4 xl:w-1/2">
                     {
                         menuArr.map(x => (
                             <li key={x.id} className="grid place-content-center hover:text-red-700">
@@ -60,9 +59,9 @@ const Navbar = () => {
                     }
                     {id === "" || id === null ? <li>Bienvenido</li>
                         :
-                        <div>
+                        <div className='flex'>
                             <li className="grid place-content-center">
-                                <Link to="/login-user" className="bg-slate-200 hover:bg-slate-300 text-black px-4 py-2 hover:scale-95 transition  text-white rounded">Inicia sesión</Link>
+                                <Link to="/login-user" className="bg-slate-200 hover:bg-slate-300 text-black px-4 py-2 hover:scale-95 transition mr-4 rounded">Inicia sesión</Link>
                             </li>
                             <li className="grid place-content-center">
                                 <Link to="/register-user" className="bg-red-600 hover:bg-red-700 px-4 py-2 hover:scale-95 transition  text-white rounded">
@@ -73,6 +72,13 @@ const Navbar = () => {
                     }
                 </ul>
             </nav>
+
+            <nav className=''>
+                <img onClick={(e) => setMobile(!mobile)} className="hover:cursor-pointer hover:scale-95 md:hidden" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAYAAACpSkzOAAAAAXNSR0IArs4c6QAAAdRJREFUSEuNVomRAyEMkzu5TrKlpJNsOrvOuAMbkM23mckkAS+WbMlEwC8RICW3BAFAS+Enxa538hl5t7zqF04jEKScZZaM1sZnbbN9CDIHBczRZXGDMGZmtKEgkYhlsigqYWElSdHMchvAGuJyMusj7l2A66+VxpXUSo8Ml3q0YE1UYlb/W49Lruq6Enuk5bkk4dX6VrMvmPFyCPlGwl7OwA3gwyJx9Z8mbPps7LOie4XyvpNd2XoD8nbmaVB6lpnlAuIrso2Mmq9czwYme4NWz3Q5+NGgBEutYqPXCh+QBjPno6h0MXxlmuq17vSKOmCm6pO8aRDdQPr0hyYJrUEHizXr6OCJ40ZwI5HqbDx15ameJHUeLqH9qKqreyVrQPYDIL+3pMpEpmQTRf0ufTSWgrtq37fDtsOO0pbRah7boQ9qhXyN5c82e8frha6kpqYLCa9opsVAGHxNcTqCTBJOGcaljKB9g7ixEUI9stS4MZ4letsY6nf4Ax1PrqvLi2F3iGk0DYPtwfhpXtN+KaMjYrtlc+zh2hh0131leZyzzv98OrY1yijx4DXd5rxK4onQJ7a1Z7sYxpPt6KpPM0mwe3nsaGL6X1cBP8a9CJxOl38n/wEcUMUpNbvs8gAAAABJRU5ErkJggg==" />
+            </nav>
+
+
+
         </div>
     )
 }
