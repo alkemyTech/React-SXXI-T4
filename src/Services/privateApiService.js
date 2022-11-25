63ca4cc4392a21d5635ad1522ad77cc460e243b6
Gabriel <gabrielpindo@gmail.com>
2022-11-25 11:18:49 -0300

    Update privateApiService.js

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,37 @@
-import axios from 'axios';
+import axios from 'axios'
+
+const baseURL = "http://ongapi.alkemy.org/api"
 
 const config = {
+    baseURL: baseURL,
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 4                //Aqui va el ID del equipo!!
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+
+const instance = axios.create(config)
+
+const Put = (url, id, values) =>{
+
+    const resp = {}
+
+    instance.get(`${url}${id ? "/"+id : ""}`, getHeaders(), values)
+    .then(res => (resp.data = res.data))
+    .catch(err => resp.error = err)
+
+    return resp
 }
 
-export default Get
+const getAuthorization = () =>{
+    const token = localStorage.getItem("token")
+    return `Baerer ${token ? token:""}`
+}
+
+const getHeaders = () =>{
+    return {
+        headers:{ Authorization: getAuthorization() }
+    }
+}
+
+export default Put
