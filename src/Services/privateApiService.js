51883ccc4bbfc6fd8f1a442b26d48ab5637cb80c
Gabriel <gabrielpindo@gmail.com>
2022-11-25 17:13:51 -0300

    Creación del método PATCH

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
+
+const baseURL = "https://ongapi.alkemy.org/api";
 
 const config = {
-    headers: {
-        Group: 4                //Aqui va el ID del equipo!!
-    }
-}
+	baseURL: baseURL,
+	headers: {
+		Group: 4, // Aqui va el ID del equipo!!
+		"content-type": "application/json",
+	},
+};
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+const instance = axios.create(config);
 
-export default Get
+const Patch = async (endpoint, body) => {
+	const response = {};
+	await instance
+		.patch(`${endpoint}`, body, getHeaders())
+		.then(res => (response.data = res.data))
+		.catch(err => (response.error = err));
+	return response;
+};
+
+const getAuthorization = () => {
+	const token = localStorage.getItem("token");
+	return `Bearer ${token}`;
+};
+
+const getHeaders = () => {
+	return {
+		headers: {
+			Authorization: getAuthorization(),
+		},
+	};
+};
+
+export {  Patch };
