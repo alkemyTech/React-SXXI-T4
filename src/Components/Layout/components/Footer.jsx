965a31f5ddbcdb8431f44f33a821883e1db55232
Gabriel <gabrielpindo@gmail.com>
2022-11-25 11:24:33 -0300

    Update Footer.jsx
    Se utilizan Rutas absolutas.

--- src/Components/Layout/components/Footer.jsx
+++ src/Components/Layout/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import FormNewsletter from "./FormNewsletter";
-import { getOrganization } from "../../../Services/Organization/ApiService"
+import { getOrganization } from "Services/Organization/ApiService"
 import { Link } from "react-router-dom";
 const Footer = () => {
 
