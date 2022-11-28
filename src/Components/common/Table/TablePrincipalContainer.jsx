5bd8f48cff397961744b347cc7907557a714f6b8
Shonakar <carlosjbordon@gmail.com>
2022-11-28 07:02:59 -0300

    add table common components

---
+++ src/Components/common/Table/TablePrincipalContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TablePrincipalContainer = ({ children }) => {
+	return <div className="container mx-auto px-4 sm:px-8 py-8">{children}</div>;
+};
+
+export default TablePrincipalContainer;
