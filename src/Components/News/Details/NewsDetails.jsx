26f9d829cee98d60e30320cc5f52d80cd3a35264
Gabriel <gabrielpindo@gmail.com>
2022-11-30 15:56:07 -0300

    Update NewsDetails.jsx

--- src/Components/News/Details/NewsDetails.jsx
+++ src/Components/News/Details/NewsDetails.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
+import { useParams } from "react-router-dom";
+import { findById } from "Services/News/NewsApiServices"
+const NewsDetails = () => {
 
-const NewsDetails = ({ details }) => {
+	const { id } = useParams("id")
+	const [details, setDetails] = useState({})
+	useEffect(() => {
+		const getNewId = async () => {
+			try {
+				const res = await findById(id)
+				setDetails(res.data.data)
+			}
+			catch (err) {
+				console.log(err)
+			}
+		}
+		getNewId()
+
+	}, [])
+
 	return (
 		<div className="w-screen h-screen flex flex-col gap-7 items-center">
 			<h1 className="text-4xl font-bold text-center pt-7">{details?.name}</h1>
