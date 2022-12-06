6da59a222be8349108ec12a649e68a2ce101e0d2
Shonakar <carlosjbordon@gmail.com>
2022-12-06 09:34:22 -0300

    adding alert to home service

--- src/Services/Home/ApiServiceHome.js
+++ src/Services/Home/ApiServiceHome.js
@@ -1,17 +1,33 @@
 import { Get, Post, Put } from "Services/privateApiService";
+import { error as errorAlert, update, success } from "utils/alerts/alerts";
 
 const homeEndpoint = "/organization";
 
 const getHome = async id => {
-	return await Get(`${homeEndpoint}/${id || ""}`);
+	const { data, error } = await Get(`${homeEndpoint}/${id || ""}`);
+	if (error) {
+		errorAlert("Error al obtener los datos de Home");
+	} else {
+		return data;
+	}
 };
 
 const postHome = async body => {
-	return await Post(`${homeEndpoint}`, body);
+	const { error } = await Post(`${homeEndpoint}`, body);
+	if (error) {
+		errorAlert("Error al crear Home");
+	} else {
+		success();
+	}
 };
 
 const putHome = async (id, body) => {
-	return await Put(`${homeEndpoint}/${id}`, body);
+	const { error } = await Put(`${homeEndpoint}/${id}`, body);
+	if (error) {
+		errorAlert("Error al modificar los datos de Home");
+	} else {
+		update();
+	}
 };
 
 export { postHome, getHome, putHome };
