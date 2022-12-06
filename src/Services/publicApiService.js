57b65de511d118d483c3045ce96d41fff0eaa3aa
Shonakar <carlosjbordon@gmail.com>
2022-12-06 09:28:36 -0300

    fix public api service

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,41 +1,40 @@
 import axios from "axios";
 
-const baseURL = "https://ongapi.alkemy.org/api";
-
-const instance = axios.create(config);
-
 const config = {
-	baseURL: baseURL,
+	baseURL: "https://ongapi.alkemy.org/public/api",
 	headers: {
 		Group: 4, // Aqui va el ID del equipo!!
 		"content-type": "application/json",
 	},
 };
 
-const getHeaders = () => {
-	return {
-		headers: {
-			"content-type": "application/json",
-		},
-	};
-};
+const instance = axios.create(config);
 
-const url = "https://ongapi.alkemy.org/public/api";
-
-const Post = (relativeUrl, body) => {
+const Get = async (endpoint, id = null) => {
 	const response = {};
 	instance
-		.post(relativeUrl, body, getHeaders)
+		.get(`${endpoint}${id ? "/" + id : ""}`)
+		.then(res => (response.data = res.data.data))
+		.catch(err => (response.error = err));
+	return response;
+};
+
+const Post = async (endpoint, body) => {
+	const response = {};
+	instance
+		.post(endpoint, body)
 		.then(res => (response.data = res.data))
 		.catch(error => (response.error = error));
 	return response;
 };
 
-const Get = id => {
-	axios
-		.get(`${url}${id?"/"+ id :""}`, config)
-		.then(res => console.log(res.data))
-		.catch(err => console.log(err));
+const Put = async (endpoint, body) => {
+	const response = {};
+	await instance
+		.get(`${endpoint},body`)
+		.then(res => (response.data = res.data))
+		.catch(err => (response.error = err));
+	return response;
 };
 
-export { Get, Post }
+export { Get, Post, Put };
