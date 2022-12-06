ac0030e449b72b71e68a0f9c9b0fee651772b081
Jorge Garcia <jorge.garcia@awsoftware.com.ar>
2022-12-06 08:58:50 -0300

    feature/ONG4-76-ExtenderServicioPeticionHTTPBaseParaNovedades: se extiende peticion http base para peticiones novedades

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,40 +2,35 @@ import axios from "axios";
 
 const baseURL = "https://ongapi.alkemy.org/api";
 
+const config = {
+	baseURL,
+	headers: {
+		"content-type": "application/json",
+	}
+};
+
 const instance = axios.create(config);
 
-const config = {
-	baseURL: baseURL,
-	headers: {
-		Group: 4, // Aqui va el ID del equipo!!
-		"content-type": "application/json",
-	},
-};
-
-const getHeaders = () => {
-	return {
-		headers: {
-			"content-type": "application/json",
-		},
-	};
-};
-
-const url = "https://ongapi.alkemy.org/public/api";
-
-const Post = (relativeUrl, body) => {
+const Post = async (relativeUrl, body) => {
 	const response = {};
-	instance
-		.post(relativeUrl, body, getHeaders)
+	await instance
+		.post(relativeUrl, body)
 		.then(res => (response.data = res.data))
 		.catch(error => (response.error = error));
 	return response;
 };
 
-const Get = id => {
-	axios
-		.get(`${url}${id?"/"+ id :""}`, config)
-		.then(res => console.log(res.data))
-		.catch(err => console.log(err));
+const Get = async (relativeUrl) => {
+	const response = {};
+	await instance
+		.get(relativeUrl)
+		.then(res => {
+			response.data = res.data;
+		})
+		.catch(error => {
+			response.error = error;
+		})
+	return response;
 };
 
 export { Get, Post }
