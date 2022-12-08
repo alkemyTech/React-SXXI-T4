710cd58d3ddc5a72e4bf5c9054df57f988fc0e49
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-12-07 22:16:14 -0300

    Implementacion suspense

--- src/Components/News/Details/NewsDetails.jsx
+++ src/Components/News/Details/NewsDetails.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import LazyImage from "Components/common/LazyImage/LazyImage";
+import imageError from "Assets/images/blog-img-02.jpg"
 
 const NewsDetails = ({ details }) => {
 	return (
 		<div className="w-screen h-screen flex flex-col gap-7 items-center">
 			<h1 className="text-4xl font-bold text-center pt-7">{details?.name}</h1>
-			<div
-				className="w-full h-96 bg-center bg-cover bg-no-repeat"
-				style={{ backgroundImage: `url("${details?.image}")` }}
+			<LazyImage
+				src={details?.image}
+				srcError={imageError}
+				classToApply={"w-full h-96 bg-center bg-cover bg-no-repeat"}
 			/>
 			<div className="w-11/12 sm:w-3/4 md:w-2/4 lg:w-2/5">
 				<div className="font-light" dangerouslySetInnerHTML={{ __html: details?.content }} />
--- src/Components/common/LazyImage/LazyImage.jsx
+++ src/Components/common/LazyImage/LazyImage.jsx
@@ -1,28 +1,54 @@
-import React, { useState } from "react";
+import React, { Suspense } from "react";
+import { Skeleton } from "@mui/material";
 
-import Skeleton from "@mui/material/Skeleton";
+const imgCache = {
+	__cache: {},
+	read(src) {
+		if (!this.__cache[src]) {
+			this.__cache[src] = new Promise(resolve => {
+				const img = new Image();
+				img.onload = () => {
+					this.__cache[src] = true;
+					resolve(this.__cache[src]);
+				};
+				img.src = src;
+			}).then(img => {
+				this.__cache[src] = true;
+			});
+		}
+		if (this.__cache[src] instanceof Promise) {
+			throw this.__cache[src];
+		}
+		return this.__cache[src];
+	},
+};
+
+const SuspenseImg = ({ src, srcError, classToApply, styleToApply }) => {
+	imgCache.read(src);
+	return (
+		<img
+			src={src}
+			className={classToApply}
+			style={styleToApply}
+			onError={e => {
+				e.currentTarget.src = srcError;
+			}}
+		/>
+	);
+};
 
 const LazyImage = ({ src, srcError, classToApply, styleToApply }) => {
-	const [isLoading, setIsLoading] = useState(true);
-
 	return (
 		<>
-			{isLoading && (
-				<>
+			<Suspense
+				fallback={
 					<div className={classToApply} style={styleToApply}>
 						<Skeleton variant="rectangular" height="100%" />
 					</div>
-				</>
-			)}
-			<img
-				src={src || srcError}
-				style={{ ...styleToApply, display: isLoading && "none" }}
-				className={classToApply}
-				onLoad={() => setIsLoading(false)}
-				onError={e => {
-					e.currentTarget.src = srcError;
-				}}
-			/>
+				}
+			>
+				<SuspenseImg src={src} srcError={srcError} classToApply={classToApply} styleToApply={styleToApply} />
+			</Suspense>
 		</>
 	);
 };
