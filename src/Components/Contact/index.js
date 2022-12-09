9d2759d24a5b91a47c5135eebad53a5479ac2e61
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-12-09 13:58:51 -0300

    Tomar correctamente datos de la organizacion

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -3,28 +3,19 @@
 import React, { useEffect, useState } from "react";
 import Title from "../Title/Title";
 import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { getOrganization } from "Services/Organization/ApiService";
 
-const initialData = {
-	address: "siempreviva 2223",
-	phone: "123456123",
-	facebookUrl: "facebook.com",
-	linkedinUrl: "linkedin.com",
-	twitterUrl: "twitter.com",
-};
-
-export default function Contact({ data }) {
+export default function Contact() {
 	const [contact, setContact] = useState({});
 
 	useEffect(() => {
-		if (!data) {
-			setContact(initialData);
-		}
+		getOrganization(setContact)
 	}, []);
 
 	return (
 		<div className="flex justify-center p-5">
 			<div className="bg-slate-100 w-full sm:w-full md:w-3/5 lg:w-1/2 shadow-xl rounded ">
-				<Title title="Contacto" />
+				<Title text="Contacto" />
 				<div className="p-4">
 					<h1 className="font-semibold">Datos de contacto</h1>
 					<div className="py-2">
@@ -32,29 +23,29 @@ export default function Contact({ data }) {
 							Direccion: <span className="font-normal">{contact?.address}</span>
 						</h3>
 						<h3 className="font-medium">
-							Telefono: <span className="font-normal">{contact?.phone}</span>
+							Telefono: <span className="font-normal">{contact?.cellphone}</span>
 						</h3>
 					</div>
 
 					<h3 className="font-semibold py-2">Nuestras redes</h3>
 					<div className="flex justify-center items-center gap-4 ">
 						<div className="flex items-center justify-center w-1/2 h-12 rounded-xl shadow text-center bg-white">
-							<a href={contact.facebookUrl}>
+							<a href={contact?.facebook_url}>
 								<FaFacebook className="text-blue-500" size={30} />
 							</a>
 						</div>
 						<div className="flex items-center justify-center w-1/2 h-12 rounded-xl shadow text-center bg-white">
-							<a href={contact.linkedinUrl}>
+							<a href={contact?.linkedin_url}>
 								<FaLinkedin className="text-blue-700" size={30} />
 							</a>
 						</div>
 						<div className="flex items-center justify-center w-1/2 h-12 rounded-xl shadow text-center bg-white">
-							<a href={contact.twitterUrl}>
+							<a href={contact.twitter_url}>
 								<FaTwitter className="text-blue-600" size={30} />
 							</a>
 						</div>
 						<div className="flex justify-center items-center w-1/2 h-12 rounded-xl shadow bg-white">
-							<a className="flex items-center" href={contact.instagramUrl}>
+							<a className="flex items-center" href={contact?.instagram_url}>
 								<img src="/images/logoig.png" width={30} height={30} alt="logo" />
 							</a>
 						</div>
