e065dac31338a1d64f3c7d7ce6a7e2f9b66d1b71
Shonakar <carlosjbordon@gmail.com>
2022-12-13 16:26:04 -0300

    home apiService

--- src/Services/Home/ApiService.js
+++ src/Services/Home/ApiService.js
@@ -1,15 +1,25 @@
 import axios from "axios";
-import { Get } from "Services/publicApiService";
-import { error as errorAlert } from "utils/alerts/alerts";
 
-const newsEndPoint = "/news";
-const membersEndpoint = "/members";
-const slidesEndpoint = "/slides";
+import { Get, Post, Put } from "Services/privateApiService";
+import { error as errorAlert, update, success } from "utils/alerts/alerts";
+
+const homeEndpoint = "/organization";
+const newsEndpoint = "/news";
+const slideEndpoint = "/slides";
 
 const getSlides = async () => {
-	const { data, error } = await Get(slidesEndpoint);
+	const { data, error } = await Get(`${slideEndpoint}`);
 	if (error) {
-		errorAlert("Error al extraer los slides");
+		errorAlert("Error al obtener los slides");
+	} else {
+		return data;
+	}
+};
+
+const getNews = async () => {
+	const { data, error } = await Get(newsEndpoint);
+	if (error) {
+		errorAlert("Error al extraer las novedades");
 	} else {
 		return data;
 	}
@@ -18,27 +28,36 @@ const getSlides = async () => {
 const putSlides = value => {
 	console.log(value.id);
 	axios
-		.put(`${slidesEndpoint}/${value.id}`, value)
+		.put(`https://ongapi.alkemy.org/api/slides/${value.id}`, value)
 		.then(res => console.log(res))
 		.catch(err => console.log(err));
 };
 
-const getNews = async () => {
-	const { data, error } = await Get(newsEndPoint);
+const getHome = async id => {
+	const { data, error } = await Get(`${homeEndpoint}/${id || ""}`);
 	if (error) {
-		errorAlert("Error al extraer las novedades");
+		errorAlert("Error al obtener los datos de Home");
 	} else {
 		return data;
 	}
 };
 
-const getMembers = async () => {
-	const { data, error } = await Get(membersEndpoint);
+const postHome = async body => {
+	const { error } = await Post(`${homeEndpoint}`, body);
 	if (error) {
-		errorAlert("Error al extraer las novedades");
+		errorAlert("Error al crear Home");
 	} else {
-		return data;
+		success();
 	}
 };
 
-export { getNews, getMembers, getSlides, putSlides };
+const putHome = async (id, body) => {
+	const { error } = await Put(`${homeEndpoint}/${id}`, body);
+	if (error) {
+		errorAlert("Error al modificar los datos de Home");
+	} else {
+		update();
+	}
+};
+
+export { getHome, getNews, getSlides, putSlides, putHome, postHome };
