c0b12d5bc12bfcf56101d6cef5633218e8f20488
Shonakar <carlosjbordon@gmail.com>
2022-12-12 23:59:09 -0300

    contactService

--- src/Services/Contact/ApiServiceContact.js
+++ src/Services/Contact/ApiServiceContact.js
@@ -1,21 +1,42 @@
 import { Get, Post, Put, Delete } from "Services/privateApiService";
+import { success, error as errorAlert, update, erase } from "utils/alerts/alerts";
 
 const contactsEndpoint = "/contacts";
 
 const createContact = async body => {
-	return await Post(`${contactsEndpoint}`, body);
+	const { error } = await Post(`${contactsEndpoint}`, body);
+	if (error) {
+		errorAlert();
+	} else {
+		success();
+	}
 };
 
 const getContact = async id => {
-	return await Get(`${contactsEndpoint}/${id || ""}`);
+	const { data, error } = await Get(`${contactsEndpoint}/${id || ""}`);
+	if (error) {
+		errorAlert();
+	} else {
+		return data;
+	}
 };
 
 const updateContact = async (id, body) => {
-	return await Put(`${contactsEndpoint}/${id}`, body);
+	const { error } = await Put(`${contactsEndpoint}/${id}`, body);
+	if (error) {
+		errorAlert();
+	} else {
+		update();
+	}
 };
 
 const deleteContact = async id => {
-	return await Delete(`${contactsEndpoint}/${id}`);
+	const { error } = await Delete(`${contactsEndpoint}/${id}`);
+	if (error) {
+		errorAlert();
+	} else {
+		erase();
+	}
 };
 
 export { createContact, getContact, updateContact, deleteContact };
