05d1d25805f18d93956aed0f07544e71bb8cddf2
Gabriel <gabrielpindo@gmail.com>
2022-12-13 22:28:49 -0300

    Update Error404.jsx

--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -4,19 +4,21 @@ import { Link } from 'react-router-dom'
 const Error404 = () => {
 
   const style = {
-    background: `url(${img})`
+    background: `url(${img})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
   }
 
   return (
     <div style={style} className="w-full h-full flex flex-col justify-center items-center">
       <div className="bg-black bg-opacity-60 w-full h-full titulo flex flex-col justify-center items-center">
-        <h1 className="text-white font-poppins text-8xl">404</h1>
-        <h1 className="text-white font-poppins text-5xl">ERROR</h1>
+        <h1 className="text-white font-poppins text-5xl text-center">ERROR</h1>
+        <h1 className="text-white font-poppins text-8xl text-center">404</h1>
         <h1 className="font-poppins text-white mt-10">PÁGINA NO ENCONTRADA</h1>
-        <Link  to="/" className="bg-sky-400 rounded py-5 px-20 text-white font-poppins hover:scale-95 transition-all">
+        <Link to="/" className="bg-sky-200 rounded py-5 px-20 text-slate-700 font-poppins hover:scale-95 transition-all">
           VOLVER A INICIO
         </Link>
-      </div>        
+      </div>
     </div>
   )
 }
