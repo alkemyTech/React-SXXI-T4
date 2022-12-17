0c37ff03739ccffa039def8efe3ba8b1568ab209
Shonakar <carlosjbordon@gmail.com>
2022-12-17 01:09:31 -0300

    render components

--- src/Components/Contact/ContactForms.test.jsx
+++ src/Components/Contact/ContactForms.test.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-const { default: ContactForm } = require("./ContactForm");
+import { render, screen } from "@testing-library/react";
+import ContactForm from "./ContactForm";
 
-test("render de formulario de contacto", async () => {
+test("Render de formulario de contacto", async () => {
 	render(<ContactForm />);
+
+	await expect(screen.findByAltText("logo"));
+	await expect(screen.findByText("¿Quieres contribuir?"));
+	await expect(screen.findByText("Donar"));
+	await expect(screen.findByText("¡Contactate con nosotros!"));
+	await expect(screen.findByPlaceholderText("Ingresa tu nombre"));
+	await expect(screen.findByPlaceholderText("Ingresa tu correo electronico"));
+	await expect(screen.findByPlaceholderText("Ingresa un telefono de contacto"));
+	await expect(screen.findByPlaceholderText("Ingresa tu mensaje.."));
+	await expect(screen.findByRole("button", { type: "submit" }));
 });
