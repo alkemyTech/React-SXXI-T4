be80976473fade5fdde17aa94a188641a4cd343e
Shonakar <carlosjbordon@gmail.com>
2022-12-17 02:33:14 -0300

    test error messages

--- src/Components/Contact/ContactForms.test.jsx
+++ src/Components/Contact/ContactForms.test.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ContactForm from "./ContactForm";
 
 test("Render de formulario de contacto", async () => {
 	render(<ContactForm />);
 
+	// Render components
 	const imageLogo = await screen.findByAltText("logo");
 	const contributeText = await screen.findByText("¿Quieres contribuir?");
 	const donateButton = await screen.findByText("Donar");
@@ -24,4 +26,24 @@ test("Render de formulario de contacto", async () => {
 	expect(inputPhone).toBeInTheDocument();
 	expect(inputMessage).toBeInTheDocument();
 	expect(submitButton).toBeInTheDocument();
+
+	// Error Messages
+	userEvent.click(submitButton);
+	// Required
+	const requireMessages = await screen.findAllByText("Este campo es requerido.");
+	expect(requireMessages.length).toBe(4);
+	// Email
+	userEvent.type(inputEmail, "NotEmail");
+	await screen.findByText("Debe ingresar un correo electronico valido.");
+	// Phone
+	userEvent.type(inputPhone, "text");
+	await screen.findByText("Debe Contener valores numericos");
+	userEvent.clear(inputPhone);
+	userEvent.type(inputPhone, "1");
+	await screen.findByText("Debe tener al menos 8 digitos");
+	userEvent.clear(inputPhone);
+	userEvent.type(inputPhone, "1234567890123");
+	await screen.findByText("Debe tener un maximo de 12 digitos");
+
+	// TODO: Success
 });
