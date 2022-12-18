cffa0d61f2c3d43e0ef823a040bc4861688b6a92
Shonakar <carlosjbordon@gmail.com>
2022-12-18 14:14:45 -0300

    button to public page (backoffice)

--- src/Components/SideBar/MenuSideBar.jsx
+++ src/Components/SideBar/MenuSideBar.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Progress from "Components/common/Loader/Progress";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaAngleLeft } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { userLogout } from "store/Slices/authSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const MenuSideBar = ({ setOpen, open }) => {
 	const dispatch = useDispatch();
@@ -18,7 +18,10 @@ const MenuSideBar = ({ setOpen, open }) => {
 	return (
 		<>
 			<div className="fixed top-0 w-full flex z-20">
-				<div className="flex justify-end sm:justify-between items-center w-full p-3  bg-sky-800 ">
+				<div className="flex justify-between items-center w-full p-3  bg-sky-800 ">
+					<div className=" font-poppins md:hidden text-white border rounded p-1 px-2">
+						<Link to="/">Inicio</Link>
+					</div>
 					<button
 						className={` hidden sm:block cursor-pointer p-2 ml-2
 								border-2 rounded-md  ${!open && "rotate-180"}`}
